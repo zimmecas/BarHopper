e7f5578987e3a7f7df0ce6326519413749be8fca
Casey M. Zimmerman <zimmecas@mail.gvsu.edu>
2016-04-21 20:04:36 -0400

    mapsScreen only worked on my phone, may work on everyones now.

--- app/src/main/java/edu/gvsu/cis/zimmecas/barhopper/mapActivities/mapsScreen.java
+++ app/src/main/java/edu/gvsu/cis/zimmecas/barhopper/mapActivities/mapsScreen.java
@@ -1,12 +1,19 @@
 package edu.gvsu.cis.zimmecas.barhopper.mapActivities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -16,8 +23,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import edu.gvsu.cis.zimmecas.barhopper.R;
 
-public class mapsScreen extends AppCompatActivity {
-
+public class mapsScreen extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
     GoogleMap myMap;
 
     @Override
@@ -27,15 +35,55 @@ public class mapsScreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
         myMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        onMapReady(myMap);
+
     }
 
-    public void onMapReady(GoogleMap googleMap) {
-        myMap = googleMap;
 
-        // Add a marker in Grand Rapids, USA,  and move the camera.
-        LatLng grandRapids = new LatLng(42.9634, 85.6681);
-        myMap.moveCamera(CameraUpdateFactory.newLatLng(grandRapids));
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //  Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            myMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude())));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
