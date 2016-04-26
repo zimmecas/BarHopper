c3425ff10bf89be91f7799467e88072ba0991803
Louis Sullivan <sullivlo@mail.gvsu.edu>
2016-04-26 19:04:43 -0400

    Added "Master/Detail Flow"

--- app/src/main/java/edu/gvsu/cis/zimmecas/barhopper/dummy/MakeCalls.java
+++ app/src/main/java/edu/gvsu/cis/zimmecas/barhopper/dummy/MakeCalls.java
@@ -1,32 +1,48 @@
 package edu.gvsu.cis.zimmecas.barhopper.dummy;
 
+import android.support.v7.widget.RecyclerView;
 
 
 import java.util.List;
 
-import edu.gvsu.cis.zimmecas.barhopper.Bar;
 import retrofit.Callback;
+import retrofit.RestAdapter;
 import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+
+import edu.gvsu.cis.zimmecas.barhopper.Bar;
+
 
 /**
  * Created by louissullivan on 4/26/16.
  */
-public class MakeCalls implements CallBack<List<WordInfo>> {
+
+public class MakeCalls implements Callback<List<WordInfo>>{
+//List<WordInfo>> {
+
+
 
     @Override
-    public void success(List<WordInfo> wordInfos, Responce responce) {
+    public void success(List<WordInfo> wordInfos, Response response) {
 
-        for (WordInfo w : WordInfo) {
-            System.out.println(w.name);
-            System.out.println(w.address);
-            System.out.println(w.name);
-            System.out.println(w.name);
+
+
+        for (WordInfo w : wordInfos) {
+
+            System.out.println(w.syllable);
+            System.out.println(w.word);
+            System.out.println(w.flags);
+            System.out.println(w.freq);
+            System.out.println(w.score);
         }
+
     }
 
     @Override
-    public void failure(RetrofitError retrofitError){
+    public void failure (RetrofitError retrofitError){
 
     }
+
 
 }
