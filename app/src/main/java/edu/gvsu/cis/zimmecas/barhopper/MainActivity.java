2822a2221ec2393ed4f5cbc9a83e0084503e4760
Louis Sullivan <sullivlo@mail.gvsu.edu>
2016-04-28 09:53:41 -0400

    Mainactivity changes and downloaded zip

--- app/src/main/java/edu/gvsu/cis/zimmecas/barhopper/MainActivity.java
+++ app/src/main/java/edu/gvsu/cis/zimmecas/barhopper/MainActivity.java
@@ -2,13 +2,9 @@ package edu.gvsu.cis.zimmecas.barhopper;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
