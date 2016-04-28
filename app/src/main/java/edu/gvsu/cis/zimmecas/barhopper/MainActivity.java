cd3426b077b5f6e1c2d9be2f2915333c24676e33
Tim DeVries <devritim@mail.gvsu.edu>
2016-04-27 22:08:53 -0400

    Added the BarListActivity

--- app/src/main/java/edu/gvsu/cis/zimmecas/barhopper/MainActivity.java
+++ app/src/main/java/edu/gvsu/cis/zimmecas/barhopper/MainActivity.java
@@ -189,11 +189,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(start);
         }
         else if (v==route) {
-            Intent start = new Intent(this, edu.gvsu.cis.zimmecas.barhopper.ItemListActivity.class);
+            Intent start = new Intent(this, ItemListActivity.class);
             startActivity(start);
         }
         else if (v==settings) {
-            Intent start = new Intent(this, edu.gvsu.cis.zimmecas.barhopper.settings.class);
+            Intent start = new Intent(this, settings.class);
             startActivity(start);
         }
         else if (v==bac) {
