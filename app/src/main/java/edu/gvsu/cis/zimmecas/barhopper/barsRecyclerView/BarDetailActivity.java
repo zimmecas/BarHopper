c559afe4ce422147801e6cd9326d089ddc88e2af
Louis Sullivan <sullivlo@mail.gvsu.edu>
2016-04-28 12:10:54 -0400

    BarDetailView goes to website

--- app/src/main/java/edu/gvsu/cis/zimmecas/barhopper/barsRecyclerView/BarDetailActivity.java
+++ app/src/main/java/edu/gvsu/cis/zimmecas/barhopper/barsRecyclerView/BarDetailActivity.java
@@ -4,15 +4,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
+import edu.gvsu.cis.zimmecas.barhopper.Bar;
 import edu.gvsu.cis.zimmecas.barhopper.MainActivity;
 import edu.gvsu.cis.zimmecas.barhopper.R;
+
 
 /**
  * An activity representing a single Bar detail screen. This
@@ -20,7 +21,9 @@ import edu.gvsu.cis.zimmecas.barhopper.R;
  * item details are presented side-by-side with a list of items
  * in a {@link BarListActivity}.
  */
-public class BarDetailActivity extends AppCompatActivity {
+public class BarDetailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Bar bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +34,9 @@ public class BarDetailActivity extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setImageResource(R.drawable.ic_action_name);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
+        fab.setOnClickListener(this);
+/*            @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Go to website", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
                 //first bar
                 //pub 43
                 String url = "http://www.mlive.com/business/west-michigan/index.ssf/2010/04/owner_of_diversions_and_pub_43.html";
@@ -44,8 +45,8 @@ public class BarDetailActivity extends AppCompatActivity {
                 //set back to openurl once it works
                 i.setData(Uri.parse(url));
                 startActivity(i);
-            }
-        });
+            }*/
+            //));
 
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
@@ -66,6 +67,8 @@ public class BarDetailActivity extends AppCompatActivity {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
             Bundle arguments = new Bundle();
+            int index = getIntent().getIntExtra("index", 0);
+            bar = MainActivity.getBars().get(index);
             arguments.putInt("index",
                     getIntent().getIntExtra("index", 0));
             BarDetailFragment fragment = new BarDetailFragment();
@@ -90,5 +93,17 @@ public class BarDetailActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+            //first bar
+            //pub 43
+            String url = "http://www.mlive.com/business/west-michigan/index.ssf/2010/04/owner_of_diversions_and_pub_43.html";
+            String openurl = "" + bar.getHref();
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            //set back to openurl once it works
+            i.setData(Uri.parse(openurl));
+            startActivity(i);
     }
 }
